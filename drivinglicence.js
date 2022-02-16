419fc6c20ded7a298b7633aa63ea4d9b950bf27f
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-02-16 11:53:37 +0530

    driving licence

---
+++ drivinglicence.js
@@ -0,0 +1,13 @@
+function shyam()
+{
+	confirm("do you want to continuee..!!")
+x=parseInt(document.getElementById("tt").value)
+if (x>18) {
+	document.getElementById("ram").innerHTML="you are eligable for driving license"
+}
+else{
+	document.getElementById("ram1").innerHTML="you are not eligable for driving license"
+}
+
+
+}
